273c6b24ae28e02ac86fe99260e35d999c7ab562
Nikhil J Alungal <nikhiljalumkal@gmail.com>
2025-07-20 22:11:19 +0530

    Added product detail page with price history graph

--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-def product_list(request):
-    products = Product.objects.all().order_by('-last_updated')
-    return render(request, 'products/product_list.html', {'products': products})
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    price_history = product.price_history.all().order_by('date')
+    return render(request, 'products/product_detail.html', {
+        'product': product,
+        'price_history': price_history
+    })
